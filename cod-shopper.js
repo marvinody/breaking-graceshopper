5d95ed7d7cec1ba732ee8e29663b26cd1e6cd80e
Manuel Bugallo <marvinody@gmail.com>
2019-11-13 12:42:33 -0500

    cod-shopper

---
+++ cod-shopper.js
@@ -0,0 +1,7 @@
+/*
+Your urls are pseudo-safe.
+You check to see if someone is logged in,
+but that's it.
+You need to check if the user that's in the url in the user that's doing the action. I can make a new user, login, and then modify other users' cart.
+No code since several routes are broken and you guys have really long urls
+*/
